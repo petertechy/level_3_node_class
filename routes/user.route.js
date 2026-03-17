ff8619ff454972bc8b6aea6dfd27fa7ef51fb5f6
Petertechy <petertechy01@gmail.com>
2026-03-17 15:09:51 +0100

    Update user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router()
-const { signUp, userDashboard, landingPage } = require("../controllers/user.controller");
+const { signUp, userDashboard, landingPage, registerUser, authenticateUser } = require("../controllers/user.controller");
 const { allProduct } = require("../controllers/product.controller");
 
 router.get("/", landingPage)
 router.get("/sign-up", signUp);
 router.get("/dashboard", userDashboard);
 router.get("/all-product", allProduct)
+router.post("/register", registerUser);
+router.post("/signin", authenticateUser)
 
 module.exports = router
