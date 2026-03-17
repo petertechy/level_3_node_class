40e58fb8c4c1ebff0be8846846a17596ffd52906
Petertechy <petertechy01@gmail.com>
2026-03-17 15:06:54 +0100

    Update user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 //User Schema
 const userSchema = mongoose.Schema({
@@ -9,7 +10,31 @@ const userSchema = mongoose.Schema({
   password: { type: String, required: true },
 });
 
+let saltRound = 10;
+
+userSchema.pre("save", function () {
+  if (!this.isModified("password")) return;
+
+  return bcrypt.hash(this.password, saltRound)
+    .then((hashedPassword) => {
+      this.password = hashedPassword;
+    })
+    .catch((err) => {
+      console.log(err, "password could not be hashed");
+      throw err; // ❗ important
+    });
+});
+
+userSchema.methods.validatePassword = function (password, callback) {
+  console.log(password, this.password);
+
+  bcrypt.compare(password, this.password, (err, same) => {
+    console.log(same);
+    callback(err, same);
+  });
+};
+
 //User Model
 const userModel = mongoose.model("users", userSchema);
 
-module.exports =  userModel
+module.exports = userModel;
