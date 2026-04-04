3b3dacb51fcaafce7d7ab30697d4fe4a372f2846
Petertechy <petertechy01@gmail.com>
2026-04-05 00:19:58 +0100

    Update user routes

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,15 +1,29 @@
 const express = require("express");
-const router = express.Router()
-const { signUp, userDashboard, landingPage, registerUser, authenticateUser, getDashboard, uploadFile } = require("../controllers/user.controller");
+const router = express.Router();
+const {
+  signUp,
+  userDashboard,
+  landingPage,
+  registerUser,
+  authenticateUser,
+  getDashboard,
+  uploadFile,
+  initializePayment,
+  verifyPayment,
+  paymentCallback,
+} = require("../controllers/user.controller");
 const { allProduct } = require("../controllers/product.controller");
 
-router.get("/", landingPage)
+router.get("/", landingPage);
 router.get("/sign-up", signUp);
 router.get("/dashboard", userDashboard);
-router.get("/all-product", allProduct)
+router.get("/all-product", allProduct);
 router.post("/register", registerUser);
-router.post("/signin", authenticateUser)
-router.get("/getdashboard", getDashboard)
-router.post("/upload", uploadFile)
+router.post("/signin", authenticateUser);
+router.get("/getdashboard", getDashboard);
+router.post("/upload", uploadFile);
+router.post("/payment/initialize", initializePayment);
+router.get("/payment/verify/:reference", verifyPayment);
+router.get("/payment/callback", paymentCallback);
 
-module.exports = router
+module.exports = router;
